2d378a86d182469b45f4e8c4330d31625e312ccd
Kavin Prakash Senthil Prabhu <kavin.senthil@itp.objectfrontier.com>
2023-10-03 15:39:30 +0530

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h>
+                <q> Hello world!!!!</q>
+                <p>The <abbr title='Doctor'>DR.</abbr>is on the way.</p>
+            </h>
+            <address>
+                <a href="kavinprakash1912@gmail.com">Kavinprakash</a>
+            </address>
+        </div>
+    )
+}
